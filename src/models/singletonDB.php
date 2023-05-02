6cfc00a2789aa1dddda3dcd62e9774e8309a8953
adrian <guiral.132137@e.unavarra.es>
2023-05-02 20:19:09 +0200

    Issue #16 finished

---
+++ src/models/singletonDB.php
@@ -0,0 +1,22 @@
+<?php
+class SingletonDB {
+    private static $instance = null;
+    private $connection;
+    private function __construct() {
+        $this->connection = mysqli_connect("localhost", "root", "", "ingweb");
+        //$this->connection = mysqli_connect("localhost", "root", "", "ingweb");
+    }
+
+    public static function getInstance(): self
+    {
+        if(self::$instance == null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
+?>
