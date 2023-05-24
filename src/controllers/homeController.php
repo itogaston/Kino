e986d1385cde67dd6b2dd419fb4e40b98e55d257
Iñigo <gaston.131765@e.unavarra.es>
2023-05-24 16:16:32 +0000

    Update homeController.php

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -1,3 +1,8 @@
 <?php
-echo "<p> Vista </p>"
+require_once "models/movieModel.php";
+
+$moviesCarousel = Movie::getAllMovies("Test");
+$showcaseMovie = Movie::getAllMovies("case")[0];
+
+require_once "views/homeView.php";
 ?>
