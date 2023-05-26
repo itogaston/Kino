7ec390de7758a83db883a01b25a835e0aab6706c
itogaston <itogaston.2@gmail.com>
2023-05-26 16:58:24 +0200

    Add logout Button

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -4,5 +4,9 @@ require_once "models/movieModel.php";
 $moviesCarousel = Movie::getAllMovies("Test");
 $showcaseMovie = Movie::getAllMovies("case")[0];
 
+
+session_start();
+$logged = isset($_SESSION['user']);
+
 require_once "views/homeView.php";
 ?>
